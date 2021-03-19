e54654d27703ef7a1ffceba3a2f74565b701c939
karan-ban <karan100601@gmail.com>
2021-03-19 20:08:25 +0530

    code added

--- multiplication/mul.py
+++ multiplication/mul.py
@@ -1,5 +1,5 @@
 def multiplication(a,b)
   return a*b
-a=int(input("enter the first no."))
-b=int(input("enter the second no."))
+a=int(input("enter the 1 st no."))
+b=int(input("enter the 2 nd no."))
 print(multiply(a,b))
